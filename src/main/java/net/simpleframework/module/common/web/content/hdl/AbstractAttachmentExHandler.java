feccd895e9f8bd2a69a142955cc51b36863da594
simpleframework <cknet@126.com>
2013-11-15 11:35:38 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/common/web/content/hdl/AbstractAttachmentExHandler.java
+++ src/main/java/net/simpleframework/module/common/web/content/hdl/AbstractAttachmentExHandler.java
@@ -31,7 +31,7 @@ import net.simpleframework.mvc.component.ext.attachments.IAttachmentHandler;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public abstract class AbstractAttachmentExHandler<T extends Attachment, M extends AbstractIdBean>
