4a9da83525d1e29419d198a8d79a2e72fc9c814c
CHENKAN <CHENKAN@CHENKAN-PC>
2015-03-24 11:45:42 +0800

    Signed-off-by: CHENKAN <CHENKAN@CHENKAN-PC>

--- src/main/java/net/simpleframework/module/common/web/page/AbstractMgrTPage.java
+++ src/main/java/net/simpleframework/module/common/web/page/AbstractMgrTPage.java
@@ -1,11 +1,5 @@
 package net.simpleframework.module.common.web.page;
 
-import net.simpleframework.common.ID;
-import net.simpleframework.ctx.permission.PermissionDept;
-import net.simpleframework.ctx.permission.PermissionUser;
-import net.simpleframework.mvc.PageParameter;
-import net.simpleframework.mvc.PageRequestResponse.IVal;
-import net.simpleframework.mvc.ctx.permission.IPagePermissionHandler;
 import net.simpleframework.mvc.template.lets.Tabs_BlankPage;
 
 /**
@@ -15,22 +9,4 @@ import net.simpleframework.mvc.template.lets.Tabs_BlankPage;
  *         http://www.simpleframework.net
  */
 public class AbstractMgrTPage extends Tabs_BlankPage {
-
-	public static PermissionDept getOrg(final PageParameter pp) {
-		return pp.getCache("@org", new IVal<PermissionDept>() {
-			@Override
-			public PermissionDept get() {
-				PermissionDept org = null;
-				final IPagePermissionHandler hdl = pp.getPermission();
-				final PermissionUser login = pp.getLogin();
-				if (login.isManager()) {
-					org = hdl.getDept(ID.of(pp.getParameter("orgId")));
-				}
-				if (org == null) {
-					org = hdl.getDept(login.getDept().getDomainId());
-				}
-				return org;
-			}
-		});
-	}
 }
