44b1073ab7de43839669aec9b3c1e6813a18257c
CHENK <CHENK@CHENK-PC>
2015-09-15 11:53:56 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/common/web/content/page/AbstractRecommendationPage.java
+++ src/main/java/net/simpleframework/module/common/web/content/page/AbstractRecommendationPage.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 import net.simpleframework.common.NumberUtils;
 import net.simpleframework.ctx.service.ado.db.IDbBeanService;
-import net.simpleframework.module.common.DescriptionLogUtils;
+import net.simpleframework.module.common.LogDesc;
 import net.simpleframework.module.common.content.AbstractRecommendContentBean;
 import net.simpleframework.mvc.JavascriptForward;
 import net.simpleframework.mvc.PageParameter;
@@ -66,7 +66,7 @@ public abstract class AbstractRecommendationPage<T extends AbstractRecommendCont
 				bean.setRecommendationDate(null);
 				bean.setRecommendationDuration(0);
 			}
-			DescriptionLogUtils.set(bean, cp.getParameter("r_description"));
+			LogDesc.set(bean, cp.getParameter("r_description"));
 			getBeanService().update(
 					new String[] { "recommendation", "recommendationDate", "recommendationDuration" },
 					bean);
--- src/main/java/net/simpleframework/module/common/web/page/AbstractDescPage.java
+++ src/main/java/net/simpleframework/module/common/web/page/AbstractDescPage.java
@@ -1,7 +1,7 @@
 package net.simpleframework.module.common.web.page;
 
 import static net.simpleframework.common.I18n.$m;
-import net.simpleframework.module.common.DescriptionLogUtils;
+import net.simpleframework.module.common.LogDesc;
 import net.simpleframework.mvc.PageParameter;
 import net.simpleframework.mvc.common.element.InputElement;
 import net.simpleframework.mvc.common.element.RowField;
@@ -28,7 +28,7 @@ public abstract class AbstractDescPage extends FormTableRowTemplatePage {
 	}
 
 	protected void setLogDescription(final PageParameter pp, final Object bean) {
-		DescriptionLogUtils.set(bean, pp.getParameter("sl_description"));
+		LogDesc.set(bean, pp.getParameter("sl_description"));
 	}
 
 	protected InputElement createTextarea(final PageParameter pp) {
